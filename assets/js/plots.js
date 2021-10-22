d707068c4c8471a8fa4772c20a7dca975bf4e6bb
RahulREg <60970896+RahulREg@users.noreply.github.com>
2021-10-23 01:44:58 +0530

    Update plots.js

--- assets/js/plots.js
+++ assets/js/plots.js
@@ -1,25 +1,6 @@
 //Bhangar,Kalimpong
-const colours = {BJP:"rgb(255, 165, 0)", AITC: "rgb(0,255,0)", NOTA: "rgb(255,0,0)", IND: "rgb(0,0,255)", RSSCMJP: "rgb(0,255,255)"}
+const colours = { BJP: "rgb(255, 165, 0)", AITC: "rgb(0,255,0)", NOTA: "rgb(255,0,0)", IND: "rgb(0,0,255)", RSSCMJP: "rgb(0,255,255)" }
 
-async function traces(url, party) {
-    const response = await fetch(url);
-    let data = await response.json();
-    const dataset = data.filter(el => el["PARTY"] === party);
-
-    const newObj = {};
-
-    newObj.x = [];
-    newObj.y = [];
-    newObj.orientation = 'h';
-    newObj.type = 'bar';
-    newObj.name = party;
-    dataset.forEach(element => {
-        newObj.x.unshift(element["% VOTES POLLED"]);
-        newObj.y.unshift(element["AC NO."] +" "+ element["AC NAME"]+ " ");
-    });
-    
-    return newObj;
-}
 
 async function pieChart(url, ac_no, id) {
     let plotData = [];
@@ -32,7 +13,7 @@ async function pieChart(url, ac_no, id) {
 
     newObj.values = [];
     newObj.labels = [];
-    newObj.marker = {colors: []}
+    newObj.marker = { colors: [] }
     let count = 0;
     newObj.type = "pie";
 
@@ -61,54 +42,68 @@ async function pieChart(url, ac_no, id) {
 }
 
 
-async function barChart() {
+async function traces(url, party) {
+    const response = await fetch(url);
+    let data = await response.json();
+    const dataset = data.filter(el => el["PARTY"] === party);
+
+    const newObj = {};
+
+    newObj.x = [];
+    newObj.y = [];
+    newObj.orientation = 'h';
+    newObj.type = 'bar';
+    newObj.name = party;
+    dataset.forEach(element => {
+        newObj.x.unshift(element["% VOTES POLLED"]);
+        newObj.y.unshift(element["AC NO."] + " " + element["AC NAME"] + " ");
+    });
+    if(Object.keys(colours).includes(party)){
+        newObj.marker = {
+            color: colours[party]
+        }
+    }
+
+    return newObj;
+}
+
+async function getData(rows) {
     let plotData = [];
-    
-    let data = await fetch("datasets/csv/allParties.csv")
-    let dataset = await data.text();
-    let rows = dataset.split("\r\n");
+
     rows.forEach(async ele => {
         let partyname = ele.split(",");
         let t = await traces("datasets/d.json", partyname[1]);
         plotData.push(t);
-        console.log("1")
     });
-    console.log("2");
+
+    return plotData;
+}
+
+
+async function barChart() {
+
+    let data = await fetch("datasets/csv/allParties.csv")
+    let dataset = await data.text();
+    let rows = dataset.split("\r\n");
+
     const layout = {
         height: 6000,
         margin: {
             l: 200,
             r: 20,
-            t: 200,
+            t: 50,
             b: 70
-          },
+        },
         barmode: 'stack'
     };
     const config = { responsive: true };
-    Plotly.newPlot(map, plotData, layout, config);
+
+    const plotData = await getData(rows);
+    setTimeout(() => {
+        Plotly.newPlot(map, plotData, layout, config);
+    }, 1000);
 }
 
 
-// async function mapChart(url) {
-//     let plotData = [];
-//     const response = await fetch(url);
-//     const data = await response.text();
-//     const rows = data.split("\n").slice(1);
-//     console.log(rows)
-//     const res = await fetch("datasets/csv/LatLong.csv");
-//     const arr = await res.text();
-//     const dataset = arr.split("\r\n");
-//     const newObj = {}
-//     newObj.lat = [];
-//     newObj.lon = [];
-//     newObj.marker = {color: []}
-//     rows.forEach(e => {
-//         const temp = e.split(",");
-        
-//     })
-// }
-
 pieChart("datasets/d.json", 1, "myDiv");
 barChart();
-
-// mapChart("datasets/csv/List_of_Successful_Candidates.csv");
