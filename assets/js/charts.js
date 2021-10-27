1637f90dde831e91ad519862fa2b8e8e847a9518
RahulREg <60970896+RahulREg@users.noreply.github.com>
2021-10-27 21:10:07 +0530

    added charts

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -34,6 +34,7 @@ async function pieChart(url, ac_n, id) {
 
     const layout = {
         showlegend: true,
+        automargin: true,
         legend: {
             "orientation": "h",
             x: 0,
@@ -55,6 +56,7 @@ async function pieChart(url, ac_n, id) {
     Plotly.newPlot(id, plotData, layout, config);
 }
 
+
 async function pc1(url, no, id) {
     let plotData = [];
     const response = await fetch(url);
@@ -62,8 +64,8 @@ async function pc1(url, no, id) {
     const dataset = data.split("@").splice(1);
     const row = dataset[no];
     const d = row.split(',').splice(1);
-    console.log(d)
-    console.log(dataset)
+    // console.log(d)
+    // console.log(dataset)
 
 
     const newObj = {};
@@ -73,7 +75,7 @@ async function pc1(url, no, id) {
     newObj.hole = 0.4;
     newObj.type = "pie";
 
-    for(let i = 1; i < d.length - 1; i++){
+    for (let i = 1; i < d.length - 1; i++) {
         newObj.values.push(d[i]);
     }
 
@@ -82,6 +84,7 @@ async function pc1(url, no, id) {
     const layout = {
         title: d[0],
         showlegend: true,
+        automargin: true,
         legend: {
             "orientation": "h",
             x: 0,
@@ -102,12 +105,14 @@ async function pc1(url, no, id) {
 
     Plotly.newPlot(id, plotData, layout, config);
 }
-async function pc2(url, id) {
+
+
+async function pc2(url, no, id) {
     let plotData = [];
     const response = await fetch(url);
     let data = await response.text();
     const dataset = data.split("@").splice(1);
-    const row = dataset[1];
+    const row = dataset[no];
     const d = row.split('\r\n').splice(1);
     // console.log(d)
     // console.log(dataset)
@@ -120,7 +125,7 @@ async function pc2(url, id) {
     newObj.hole = 0.4;
     newObj.type = "pie";
 
-    for(let i = 1; i < d.length - 1; i++){
+    for (let i = 1; i < d.length - 1; i++) {
         let t = d[i].split(",");
         newObj.values.push(t[5]);
         newObj.labels.push(t[1]);
@@ -131,10 +136,11 @@ async function pc2(url, id) {
     const layout = {
         title: d[0],
         showlegend: true,
+        automargin: true,
         legend: {
             "orientation": "h",
             x: 0,
-            y: 1.5
+            y: -0.3
         },
         font: {
             family: 'Lato, sans-serif',
@@ -151,6 +157,54 @@ async function pc2(url, id) {
 
     Plotly.newPlot(id, plotData, layout, config);
 }
+
+
+async function bc1(url, no, id) {
+    let plotData = [];
+    const response = await fetch(url);
+    let data = await response.text();
+    const d = data.split('\r\n').splice(1);
+    // console.log(d)
+
+    let t = d[no].split(",");
+    // console.log(t)
+
+
+    const newObj = {};
+
+    newObj.x = ["male", "female", "third gender"];
+    newObj.y = [t[1], t[2], t[3]];
+    newObj.hole = 0.4;
+    newObj.type = "bar";
+
+
+    plotData.push(newObj);
+
+    const layout = {
+        title: t[0],
+        showlegend: false,
+        automargin: true,
+        legend: {
+            "orientation": "h",
+            x: 0,
+            y: -0.3
+        },
+        font: {
+            family: 'Lato, sans-serif',
+            color: 'rgba(245,246,249,1)'
+        },
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)'
+    };
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    };
+
+
+    Plotly.newPlot(id, plotData, layout, config);
+}
+
 
 async function traces(url, party) {
     const response = await fetch(url);
@@ -166,9 +220,9 @@ async function traces(url, party) {
     newObj.name = party;
     dataset.forEach(element => {
         newObj.x.unshift(element["% VOTES POLLED"]);
-        newObj.y.unshift(element["AC NO."] + " " + element["AC NAME"] + " ");
+        newObj.y.unshift(element["AC NAME"] + " ");
     });
-    if(Object.keys(colours).includes(party)){
+    if (Object.keys(colours).includes(party)) {
         newObj.marker = {
             color: colours[party]
         }
@@ -185,7 +239,7 @@ async function barChart(Title) {
     let dataset = await data.text();
     let rows = dataset.split("\r\n");
 
-    for(let i = 0; i < rows.length; i++) {
+    for (let i = 0; i < rows.length; i++) {
         let partyname = rows[i].split(",");
         let t = await traces("../datasets/d.json", partyname[1]);
         plotData.push(t);
@@ -199,10 +253,10 @@ async function barChart(Title) {
             l: 140,
             r: 15
         },
-        font:{
+        font: {
             family: 'Lato, sans-serif',
             color: 'rgba(245,246,249,1)'
-          },
+        },
         barmode: 'stack',
         paper_bgcolor: 'rgba(0,0,0,0)',
         plot_bgcolor: 'rgba(0,0,0,0)',
@@ -220,5 +274,16 @@ async function barChart(Title) {
 pieChart("../datasets/d.json", "Mekliganj", "myDiv");
 pc1("../datasets/csv/Electors_Data_Summary.csv", 0, "eds1");
 pc1("../datasets/csv/Electors_Data_Summary.csv", 6, "eds2");
-// pc2("../datasets/csv/Electors_Data_Summary.csv", "eds2");
+pc1("../datasets/csv/Electors_Data_Summary.csv", 7, "eds3");
+pc1("../datasets/csv/Electors_Data_Summary.csv", 8, "eds4");
+pc1("../datasets/csv/Electors_Data_Summary.csv", 9, "eds5");
+pc1("../datasets/csv/Electors_Data_Summary.csv", 10, "eds6");
+pc2("../datasets/csv/Electors_Data_Summary.csv", 1, "eds7");
+pc2("../datasets/csv/Electors_Data_Summary.csv", 2, "eds8");
+pc2("../datasets/csv/Electors_Data_Summary.csv", 3, "eds9");
+pc2("../datasets/csv/Electors_Data_Summary.csv", 4, "eds10");
+pc2("../datasets/csv/Electors_Data_Summary.csv", 5, "eds11");
+bc1("../datasets/csv/Highlight.csv", 0, "eds12");
+bc1("../datasets/csv/Highlight.csv", 1, "eds13");
+bc1("../datasets/csv/Highlight.csv", 2, "eds14");
 barChart("Constituency Results in '%'");
