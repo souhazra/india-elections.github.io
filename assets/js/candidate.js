0857ff912a37cd847494d822b4dbbec500f10e5f
RahulREg <60970896+RahulREg@users.noreply.github.com>
2021-10-31 18:56:49 +0530

    more graphs

---
+++ assets/js/candidate.js
@@ -0,0 +1,58 @@
+async function bc1(url, no, id) {
+    let plotData = [];
+    const response = await fetch(url);
+    let data = await response.text();
+    const dataset = data.split("<br>");
+    const rows = dataset[no].split("\r\n").splice(1);
+    // console.log(rows)
+    // console.log(dataset)
+
+
+    for (let i = 1; i < rows.length - 1; i++) {
+        const newObj = {};
+
+        newObj.y = [];
+        newObj.x = ["General", "SC", "ST"];
+        newObj.type = "bar";
+
+        let d = rows[i].split(',');
+        // console.log(d);
+        newObj.name = d[0];
+        for (let j = 1; j < d.length - 1; j++) {
+            newObj.y.push(d[j]);
+        }
+        plotData.push(newObj);
+    }
+
+
+    const layout = {
+        title: rows[0],
+        hovermode: 'closest',
+        showlegend: true,
+        automargin: true,
+        legend: {
+            "orientation": "h",
+            x: 0,
+            y: -0.3
+        },
+        font: {
+            family: 'Lato, sans-serif',
+            color: 'rgba(245,246,249,1)'
+        },
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)'
+    };
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    };
+
+
+    Plotly.newPlot(id, plotData, layout, config);
+}
+
+bc1("datasets/csv/Candidate_data_summary.csv", 0, can1);
+bc1("datasets/csv/Candidate_data_summary.csv", 1, can2);
+bc1("datasets/csv/Candidate_data_summary.csv", 2, can3);
+bc1("datasets/csv/Candidate_data_summary.csv", 3, can4);
+bc1("datasets/csv/Candidate_data_summary.csv", 4, can5);
